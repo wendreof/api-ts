926e970bcc0cd7a1cb1bff4ec2c921152278d920
Wendreo L. Fernandes <wendreo@outlook.com>
2019-09-04 11:21:50 -0300

    Update index.js

--- tokenGenerator/index.js
+++ tokenGenerator/index.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 let payload = {
-    iss: 'wendreo.design',
-    iat: new Date().getSeconds(),
-    exp: new Date().setMinutes(60),
-    name: 'Wendreo L. Fernandes',
-    email: 'wendreolf@gmail.com'
+  iss: 'wendreo.design',
+  iat: new Date().getSeconds(),
+  exp: new Date().setMinutes(60),
+  name: 'Wendreo L. Fernandes',
+  email: 'wendreolf@gmail.com'
 };
 
 var token = jwt.sign(payload, "batman batman batman");
